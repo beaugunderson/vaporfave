387f20d33143b2cb1f0fae54b9e44fabc2cabc58
Beau Gunderson <beau@beaugunderson.com>
2014-10-01 13:50:22 -0700

    Fix fonts on Ubuntu, operationalize

--- vaporfave.js
+++ vaporfave.js
@@ -1,44 +1,76 @@
 'use strict';
 
-var _ = require('lodash');
-
-//if (_.random(0, 1, true) < 0.01) {
-//  process.exit(0);
-//}
-
 var EccoJam = require('./lib/ecco-jam.js');
+var fs = require('fs');
 var Twit = require('twit');
 var updateWithMedia = require('./lib/twitter-update-with-media.js');
+var _ = require('lodash');
 
 //var SCREEN_NAME = process.env.SCREEN_NAME;
 
 _.mixin(Twit.prototype, updateWithMedia);
 
-var T = new Twit({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
+var program = require('commander');
 
-var eccoJam = new EccoJam();
-
-eccoJam.initialize(512 * 2, 255 * 2);
-eccoJam.draw();
-
-eccoJam.toBuffer(function (err, buffer) {
-  if (err) {
-    throw err;
-  }
-
-  T.updateWithMedia('', null, buffer, function (err, response, body) {
-    if (err || response.statusCode !== 200) {
-      return console.log('TUWM error', err, body);
+program
+  .command('tweet')
+  .description('Generate and tweet an image')
+  .option('-r, --random', 'only post a percentage of the time')
+  .action(function (options) {
+    if (options.random) {
+      if (_.random(0, 1, true) < 0.01) {
+        process.exit(0);
+      }
     }
 
-    console.log('TUWM OK');
+    var eccoJam = new EccoJam();
+
+    eccoJam.initialize(512 * 2, 255 * 2);
+    eccoJam.draw();
+
+    eccoJam.toBuffer(function (err, buffer) {
+      if (err) {
+        throw err;
+      }
+
+      console.log('Tweeting...');
+
+      var T = new Twit({
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+      });
+
+      T.updateWithMedia('', null, buffer, function (err, response, body) {
+        if (err || response.statusCode !== 200) {
+          return console.log('TUWM error', err, body);
+        }
+
+        console.log('TUWM OK');
+      });
+    });
   });
-});
+
+program
+  .command('save <filename>')
+  .description('Generate and save an image')
+  .action(function (filename) {
+    var eccoJam = new EccoJam();
+
+    eccoJam.initialize(512 * 2, 255 * 2);
+    eccoJam.draw();
+
+    eccoJam.toBuffer(function (err, buffer) {
+      if (err) {
+        throw err;
+      }
+
+      fs.writeFileSync(filename, buffer);
+    });
+  });
+
+program.parse(process.argv);
 
 //var stream = T.stream('user');
 //
