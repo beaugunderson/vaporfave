0b5b8d76da5f814db1e923891076cf1ae826f222
Beau Gunderson <beau@beaugunderson.com>
2014-10-11 11:10:15 -0700

    Use bot-utilities

--- vaporfave.js
+++ vaporfave.js
@@ -1,15 +1,13 @@
 'use strict';
 
+var botUtilities = require('bot-utilities');
 var EccoJam = require('./lib/ecco-jam.js');
 var fs = require('fs');
 var Twit = require('twit');
-var updateWithMedia = require('./lib/twitter-update-with-media.js');
-var utilities = require('./lib/utilities.js');
 var _ = require('lodash');
 
-utilities.mixin(_);
-
-_.mixin(Twit.prototype, updateWithMedia);
+_.mixin(botUtilities.lodashMixins);
+_.mixin(Twit.prototype, botUtilities.twitMixins);
 
 var program = require('commander');
 
@@ -36,31 +34,12 @@ program
         throw err;
       }
 
-      console.log('Tweeting...');
-
-      var T = new Twit({
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token: process.env.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-      });
-
-      var imageBots = [
-        '@plzrevisit',
-        '@pixelsorter',
-        '@a_quilt_bot',
-        '@badpng'
-      ];
-
-      var arrows = [
-        '→',
-        '👋'
-      ];
+      var T = new Twit(botUtilities.getTwitterAuthFromEnv());
 
       var tweet = '';
 
       if (_.percentChance(25)) {
-        tweet = _.sample(arrows) + ' ' + _.sample(imageBots);
+        tweet = botUtilities.heyYou(botUtilities.imageBot());
       }
 
       T.updateWithMedia(tweet, null, buffer, function (err, response, body) {
@@ -92,24 +71,3 @@ program
   });
 
 program.parse(process.argv);
-
-//var stream = T.stream('user');
-//
-//stream.on('tweet', function (tweet) {
-//  // Discard tweets where we're not mentioned
-//  if (!_.some(tweet.entities.user_mentions, {screen_name: SCREEN_NAME})) {
-//    return;
-//  }
-//
-//  // Discard mentions where there's no media
-//  if (!tweet.entities || !tweet.entities.media) {
-//    return;
-//  }
-//
-//  tweet.entities.media.forEach(function (media) {
-//    //T.updateWithMedia('@' + tweet.user.screen_name, tweet.id_str, result,
-//    //    function (err, response, body) {
-//    //  console.log('TUWM status', err, response.statusCode, body);
-//    //});
-//  });
-//});
